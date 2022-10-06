f529e3475e5cd77a469dc4394f82969c481fc5fb
Alfonso Bravo <alfonso.bravo.molina@gmail.com>
2022-10-06 12:47:33 +0200

    Add hooks and API conexion

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import {useState, useEffect} from 'react'
+import axios from 'axios';
 import './App.css';
 import Header from './components/ui/Header';
+import CharacterGrid from './components/characters/CharacterGrid';
 
 const App = () => {
+
+  const [characters, setCharacters] = useState([])
+  const [isLoading, setIsLoading] = useState (true)
+
+  useEffect(
+    () => {
+      const fetchItems = async () => {
+        const result = await axios(
+          `https://www.breakingbadapi.com/api/characters`
+        )
+
+        console.log(result.data)
+
+        setCharacters(result.data)
+        setIsLoading(false)
+      }
+
+      fetchItems()
+    }, []
+  )
+
   return (
     <div className="container">
       <Header />
+      <CharacterGrid isLoading={isLoading} characters={characters} />
     </div>
   );
 }
