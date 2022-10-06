e701f34e04d1502d401ea7d10c96cffa22f6b318
Alfonso Bravo <alfonso.bravo.molina@gmail.com>
2022-10-06 13:28:52 +0200

    Create CharacterItem and change CharacterGrid to include it.

--- src/components/characters/CharacterGrid.jsx
+++ src/components/characters/CharacterGrid.jsx
@@ -1,10 +1,18 @@
+import CharacterItem from "./CharacterItem"
+
 const CharacterGrid = ({isLoading, characters}) => {
 
-    return (
-        isLoading ? (<h1>Loading...</h1>) : (<section className="cards">
-            {characters.map((item) => (<h1 key={item.char_id}>{item.name}</h1>))}
-        </section>)
+  return (
+    isLoading ? (
+      <h1>Loading...</h1>
+    ) : (
+    <section className="cards">
+      {characters.map((item) => (
+          <CharacterItem key={item.char_id} item={item}></CharacterItem>
+      ))}
+    </section>
     )
+  )
 }
 
 export default CharacterGrid
